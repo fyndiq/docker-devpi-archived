1a092404744d1bc1dde70f5abcdbddc0f8a10fc8
Arthur Koziel <arthur@arthurkoziel.com>
2016-08-01 13:19:30 +0200

    move settings to env vars

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,36 +1,40 @@
 #!/bin/sh
 set -e
 
-export DEVPI_SERVERDIR=/data/server
-export DEVPI_CLIENTDIR=/data/client
-export DEVPI_USER="dev"
+export DEVPI_PORT="3141"
+export DEVPI_SERVERDIR="/data/server"
+export DEVPI_CLIENTDIR="/data/client"
+export DEVPI_ROOT_PASSWORD=""
+export DEVPI_USER="fyndiq"
+export DEVPI_USER_PASSWORD="fyndiq"
+export DEVPI_USER_INDEX="dev"
 
 if [ "$1" = "devpi-server" ]; then
     if [ ! -f "${DEVPI_SERVERDIR}/.serverversion" ]; then
-        echo "Initializing devpi-server"
+        echo "[RUN]: Initializing devpi-server"
 
         # start server and connect
-        devpi-server --start --host 127.0.0.1 --port 3141
+        devpi-server --start --host 127.0.0.1 --port ${DEVPI_PORT}
         devpi-server --status
-        devpi use http://localhost:3141
+        devpi use http://localhost:${DEVPI_PORT}
 
         # set password for root user
-        devpi login root --password=''
-        devpi user -m root password=''
+        devpi login root --password=${DEVPI_ROOT_PASSWORD}
+        devpi user -m root password=${DEVPI_ROOT_PASSWORD}
         devpi logoff
 
-        # register new user and create index
-        devpi user -c fyndiq password=fyndiq
-        devpi login fyndiq --password=fyndiq
-        devpi index -c dev bases=root/pypi
+        # register new user with create index
+        devpi user -c ${DEVPI_USER} password=${DEVPI_USER_PASSWORD}
+        devpi login ${DEVPI_USER} --password=${DEVPI_USER_PASSWORD}
+        devpi index -c ${DEVPI_USER_INDEX} bases=root/pypi
 
         # stop server
         devpi-server --stop
         devpi-server --status
     fi
 
-    echo "Starting devpi-server"
-    exec devpi-server --restrict-modify root --host 0.0.0.0 --port 3141
+    echo "[RUN]: Starting devpi-server"
+    exec devpi-server --restrict-modify root --host 0.0.0.0 --port ${DEVPI_PORT}
 fi
 
 exec "$@"
