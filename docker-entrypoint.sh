a183b92c5657630ffcb41a36df66e9d3b94fa278
Arthur Koziel <arthur@arthurkoziel.com>
2016-08-01 12:57:43 +0200

    changed entrypoint to create a user index

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,25 +3,34 @@ set -e
 
 export DEVPI_SERVERDIR=/data/server
 export DEVPI_CLIENTDIR=/data/client
-export DEVPI_HOST="0.0.0.0"
-export DEVPI_PORT=3141
+export DEVPI_USER="dev"
 
 if [ "$1" = "devpi-server" ]; then
     if [ ! -f "${DEVPI_SERVERDIR}/.serverversion" ]; then
         echo "Initializing devpi-server"
 
-        devpi-server --restrict-modify root --start --host "${DEVPI_HOST}" --port "${DEVPI_PORT}"
+        # start server and connect
+        devpi-server --start --host 127.0.0.1 --port 3141
         devpi-server --status
-        devpi use "http://${DEVPI_HOST}:${DEVPI_PORT}"
+        devpi use http://localhost:3141
+
+        # set password for root user
         devpi login root --password=''
-        devpi user -m root password="${DEVPI_PASSWORD}"
-        devpi index -y -c public pypi_whitelist='*'
+        devpi user -m root password=''
+        devpi logoff
+
+        # register new user and create index
+        devpi user -c fyndiq password=fyndiq
+        devpi login fyndiq --password=fyndiq
+        devpi index -c dev bases=root/pypi
+
+        # stop server
         devpi-server --stop
         devpi-server --status
     fi
 
     echo "Starting devpi-server"
-    exec devpi-server --restrict-modify root --host "$DEVPI_HOST" --port "$DEVPI_PORT"
+    exec devpi-server --restrict-modify root --host 0.0.0.0 --port 3141
 fi
 
 exec "$@"
