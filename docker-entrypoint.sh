270462bddb58a6859e8d1546f8d0ffa5a24777e7
Arthur Koziel <arthur@arthurkoziel.com>
2016-08-02 17:34:27 +0200

    dont hardcode env var values

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,32 +1,32 @@
 #!/bin/sh
 set -e
 
-export DEVPI_PORT="3141"
-export DEVPI_SERVERDIR="/data/server"
-export DEVPI_CLIENTDIR="/data/client"
-export DEVPI_ROOT_PASSWORD=""
-export DEVPI_USER="fyndiq"
-export DEVPI_USER_PASSWORD="fyndiq"
-export DEVPI_USER_INDEX="dev"
+: "${DEVPI_PORT:=3141}"
+: "${DEVPI_SERVERDIR:=/data/server}"
+: "${DEVPI_CLIENTDIR:=/data/client}"
+: "${DEVPI_ROOT_PASSWORD:=}"
+: "${DEVPI_USER:=testuser}"
+: "${DEVPI_USER_PASSWORD:=}"
+: "${DEVPI_USER_INDEX:=dev}"
 
 if [ "$1" = "devpi-server" ]; then
     if [ ! -f "${DEVPI_SERVERDIR}/.serverversion" ]; then
         echo "[RUN]: Initializing devpi-server"
 
         # start server and connect
-        devpi-server --start --host 127.0.0.1 --port ${DEVPI_PORT}
+        devpi-server --start --host 127.0.0.1 --port "${DEVPI_PORT}"
         devpi-server --status
-        devpi use http://localhost:${DEVPI_PORT}
+        devpi use http://localhost:"${DEVPI_PORT}"
 
         # set password for root user
-        devpi login root --password=${DEVPI_ROOT_PASSWORD}
-        devpi user -m root password=${DEVPI_ROOT_PASSWORD}
+        devpi login root --password=""
+        devpi user -m root password="${DEVPI_ROOT_PASSWORD}"
         devpi logoff
 
-        # register new user with create index
-        devpi user -c ${DEVPI_USER} password=${DEVPI_USER_PASSWORD}
-        devpi login ${DEVPI_USER} --password=${DEVPI_USER_PASSWORD}
-        devpi index -c ${DEVPI_USER_INDEX} bases=root/pypi
+        # register new user and create index
+        devpi user -c "${DEVPI_USER}" password="${DEVPI_USER_PASSWORD}"
+        devpi login "${DEVPI_USER}" --password="${DEVPI_USER_PASSWORD}"
+        devpi index -c "${DEVPI_USER_INDEX}" bases=root/pypi
 
         # stop server
         devpi-server --stop
@@ -34,7 +34,7 @@ if [ "$1" = "devpi-server" ]; then
     fi
 
     echo "[RUN]: Starting devpi-server"
-    exec devpi-server --restrict-modify root --host 0.0.0.0 --port ${DEVPI_PORT}
+    exec devpi-server --restrict-modify root --host 0.0.0.0 --port "${DEVPI_PORT}"
 fi
 
 exec "$@"
